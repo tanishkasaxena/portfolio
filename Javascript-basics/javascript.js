a65726e030a2b2e2077cd0517ffe8db2f95069de
Tanishka Saxena <147753065+tanishkasaxena@users.noreply.github.com>
2025-05-17 19:37:33 +0530

    Add files via upload

---
+++ Javascript-basics/javascript.js
@@ -0,0 +1,9 @@
+alert("Welcome to my portfolio!")
+console.log("Hey there!")
+
+let element = document.getElementsByClassName("intro");
+console.dir(element);
+console.log(element);
+
+let div = document.querySelector(".education");
+console.dir(div);
